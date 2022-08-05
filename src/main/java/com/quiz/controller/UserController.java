53223f614de94fb5fee60b09a98f2ccad7af4da7
HwangJaeHwan <58110333+HwangJaeHwan@users.noreply.github.com>
2022-08-05 22:51:12 +0900

    닉네임, 비밀번호 컨트롤러 (테스트 x)

--- src/main/java/com/quiz/controller/UserController.java
+++ src/main/java/com/quiz/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.quiz.controller;
 
+import com.quiz.auth.UserInfo;
+import com.quiz.request.NicknameUpdate;
+import com.quiz.request.PasswordUpdate;
 import com.quiz.request.UserCreate;
 import com.quiz.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +26,20 @@ public class UserController {
     void register(@RequestBody @Valid UserCreate userCreate) {
 
         userService.save(userCreate);
+
+    }
+
+    @PostMapping("/passwordChange")
+    void passwordChange(@AuthenticationPrincipal UserInfo userInfo, @RequestBody @Valid PasswordUpdate passwordUpdate) {
+
+        userService.passwordChange(userInfo.getUser().getId(), passwordUpdate);
+
+    }
+
+    @PostMapping("/nicknameChange")
+    void nicknameChange(@AuthenticationPrincipal UserInfo userInfo, @RequestBody @Valid NicknameUpdate nicknameUpdate) {
+
+        userService.nicknameChange(userInfo.getUser().getId(), nicknameUpdate);
 
     }
 
