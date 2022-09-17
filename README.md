61a3ec8b8fb970c8595c96a60aeadcb54e226726
HwangJaeHwan <58110333+HwangJaeHwan@users.noreply.github.com>
2022-09-17 20:52:24 +0900

    리드미 수정 및 테스트 수정

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # quiz
+[퀴즈 api](C:\Users\Hwang\work\quiz\src\main\resources\static\docs\index.html)
--- src/test/java/com/quiz/controller/docs/CommentControllerDocTest.java
+++ src/test/java/com/quiz/controller/docs/CommentControllerDocTest.java
@@ -1,4 +1,4 @@
-package com.quiz.controller;
+package com.quiz.controller.docs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.quiz.auth.UserInfo;
@@ -11,16 +11,21 @@ import com.quiz.repository.QuestionRepository;
 import com.quiz.repository.QuizRepository;
 import com.quiz.repository.UserRepository;
 import com.quiz.request.CommentCreate;
-import com.quiz.request.QuizCreate;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.restdocs.request.RequestDocumentation;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -30,7 +35,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -39,8 +46,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @Slf4j
 @SpringBootTest
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "api.quiz.com", uriPort = 443)
+@ExtendWith(RestDocumentationExtension.class)
 @AutoConfigureMockMvc
-class CommentControllerTest {
+class CommentControllerDocTest {
 
     @Autowired
     MockMvc mockMvc;
@@ -99,7 +108,7 @@ class CommentControllerTest {
 
         quizRepository.save(quiz);
 
-        IntStream.rangeClosed(1,100).forEach(
+        IntStream.rangeClosed(1,5).forEach(
                 i->{
                     list.add(QuizComment.builder()
                             .content("content" + i)
@@ -108,7 +117,7 @@ class CommentControllerTest {
                             .build());
 
                     try {
-                        Thread.sleep(2);
+                        Thread.sleep(1);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
@@ -119,12 +128,19 @@ class CommentControllerTest {
         commentRepository.saveAll(list);
 
 
-        mockMvc.perform(get("/comment/quiz/{quizId}", quiz.getId()))
+        mockMvc.perform(RestDocumentationRequestBuilders.get("/comment/quiz/{quizId}", quiz.getId()))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].id").value(100L))
-                .andExpect(jsonPath("$[0].content").value("content100"))
-                .andExpect(jsonPath("$[0].nickname").value("nickname"))
+                .andDo(document("quiz-comment-list",
+                        pathParameters(
+                                parameterWithName("quizId").description("퀴즈 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("[].id").description("댓글 ID"),
+                                fieldWithPath("[].content").description("댓글 내용"),
+                                fieldWithPath("[].nickname").description("작성자 닉네임")
 
+                        )
+                ))
                 .andDo(print());
 
 
@@ -155,9 +171,17 @@ class CommentControllerTest {
 
         String json = objectMapper.writeValueAsString(commentCreate);
 
-        mockMvc.perform(post("/comment/quiz/{quizId}", quiz.getId())
+        mockMvc.perform(RestDocumentationRequestBuilders.post("/comment/quiz/{quizId}", quiz.getId())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
+                .andDo(document("quiz-comment-create",
+                        pathParameters(
+                                parameterWithName("quizId").description("퀴즈 ID")
+                        ),
+                        requestFields(
+                                fieldWithPath("content").description("댓글 내용")
+                        )
+                        ))
                 .andExpect(status().isOk())
                 .andDo(print());
 
